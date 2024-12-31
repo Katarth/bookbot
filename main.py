7a404b5ee1b82a17649bb8d8a76bf2b5795864b4
Katarth <ccase@mail.com>
2024-12-30 18:18:57 -0800

    Finished bookbot

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ with open("books/frankenstein.txt") as f:
 def word_count(file_contents):
     words = file_contents.split()
     word_number = len(words)
-    print(word_number)
+    return(word_number)
 
 def char_count(file_contents):
     count = {}
@@ -15,5 +15,21 @@ def char_count(file_contents):
             count[char] += 1
         else:
             count[char] = 1
-    print(count)
-char_count(file_contents)
+    return(count)
+
+def book_report(word_number, count):
+    filtered_count = {}
+    for key, value in count.items():
+        if key.isalpha():
+            filtered_count[key] = value
+    filtered_list = list(filtered_count.items())
+    sorted_list = sorted(filtered_list, key=lambda item: item[1], reverse=True)
+    print(f"--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_number} words found in the document\n")
+    for char, num in sorted_list:
+        print(f"The '{char}' charcter was found {num} times")
+    print("--- End Report ---")
+
+word_number = word_count(file_contents)
+character_count = char_count(file_contents)
+book_report(word_number, character_count)
